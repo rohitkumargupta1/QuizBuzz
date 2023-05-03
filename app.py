e27e829dacf37e2f0738e3f15669c4210b26e7d6
rohitkumargupta1 <rohitguptajh@gmail.com>
2023-05-03 12:12:25 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app.secret_key = 'secret_key'
 
 
 # db connection [Postgresql]
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/flasksql'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/flasksql'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://quizbuzzflask_user:RnF4OyIy5KLOQnbmhQVQvfgbkZQr4TzZ@dpg-ch8bg0dgk4q7lmp1udfg-a.oregon-postgres.render.com/quizbuzzflask'
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"] # DATABASE_URL = render internal connection url 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"] # DATABASE_URL = render internal connection url 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
@@ -35,5 +35,5 @@ if __name__ == '__main__':
     # uncomment for model changes
     # db.drop_all()
     # db.create_all()  
-    print("All good!!!!!!!!!")
+    # print("All good!!!!!!!!!")
     app.run(debug=True)
